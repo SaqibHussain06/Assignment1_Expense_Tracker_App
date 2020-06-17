f5ff07afc0c9d98ed9d4a6396b89b72a31251015
SaqibHussain06 <65915320+SaqibHussain06@users.noreply.github.com>
2020-06-18 00:32:39 +0500

    Remove transaction Import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import {IncomeExpenses} from './components/IncomeExpenses';
 import {TransactionList} from './components/TransactionList';
 import {AddTransaction} from './components/AddTransaction';
 import {GlobalProvider} from './context/GlobalState';
-import {Transaction} from './components/Transaction';
 
 import './App.css';
 
